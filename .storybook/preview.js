75b3932835565831543d234d5537182838bf0ea7
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-06-30 18:19:51 +0100

    mocking rest

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import { addDecorator } from '@storybook/vue3'
+import { initializeWorker, mswDecorator } from 'msw-storybook-addon';
+initializeWorker();
+addDecorator(mswDecorator);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
